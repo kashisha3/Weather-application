f1a69ba890e6c33fd494a4e8bc919df16be1edd5
Kashish <arorakashish311@gmail.com>
2023-08-09 01:22:03 +0530

    Intial Commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "3750806347msh7dfbe24645deaedp18f0f2jsn2fc8525e1a3e",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
+};
+
+const preDefinedCityList = document.getElementById("pre-defined-city-list");
+
+preDefinedCityList.addEventListener("click", (e) => {
+  getWeather(e.target.name);
+});
+
+const getWeather = (city) => {
+  cityName.innerHTML = city;
+  fetch(
+    "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      cloud_pct.innerHTML = response.cloud_pct;
+      temp.innerHTML = response.temp;
+      temp2.innerHTML = response.temp;
+      feels_like.innerHTML = response.feels_like;
+      humidity.innerHTML = response.humidity;
+      humidity2.innerHTML = response.humidity;
+      min_temp.innerHTML = response.min_temp;
+      max_temp.innerHTML = response.max_temp;
+      wind_speed.innerHTML = response.wind_speed;
+      wind_speed2.innerHTML = response.wind_speed;
+      //wind_degrees.innerHTML = response.wind_degrees;
+      sunrise.innerHTML = response.sunrise;
+      sunset.innerHTML = response.sunset;
+    })
+
+    .catch((err) => console.error(err));
+};
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  getWeather(city.value);
+});
+
+getWeather("Delhi");
